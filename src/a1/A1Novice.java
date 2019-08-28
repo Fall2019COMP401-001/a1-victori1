a161850443712a4527c080452551e5b5a86b890e
victori1 <victori1@victori1-pc.wireless-1x.unc.edu>
2019-08-28 08:28:28 -0400

    boo

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,35 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
+
+		int[] ar = new int[count];
+	
+		for (int i=0; i<ar.length; i++) {
+			ar[i] = scan.nextInt();	
+			System.out.println(ar[i]);
+		}
+		scan.close();
+		
+		System.out.println(scan.next());
+		//int first = findFirstLetter(ar);
+		//int last = findLastName(ar);
+		//int total = calculateTotal(ar);
+		
+		// Print results
+		
+		//System.out.println(first + ". " + last + ": " + total);
+
+		//static int findFirstLetter(int[] ar) {
+			
+			int sum = 0;
+			
+			for (int i=0; i<ar.length; i++) {
+				sum += ar[i];
+			}
+			
+		//	return sum;
+		}
 		
 	}
-}
+
